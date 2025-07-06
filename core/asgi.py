dbd15d40baba5f985b22995b914d0df3335f6459
VohobjonovSadorbek <vohobjonovsardorbek2005@gmail.com>
2025-07-06 22:14:21 +0500

    some changed

--- core/asgi.py
+++ core/asgi.py
@@ -1,12 +1,12 @@
-
 import os
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-import main.routing  # yangi routing fayl
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+import main.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
