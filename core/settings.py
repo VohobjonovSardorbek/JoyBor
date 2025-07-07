02553425b26dd62d502f40617dac5e090cc9f675
VohobjonovSadorbek <vohobjonovsardorbek2005@gmail.com>
2025-07-07 19:05:19 +0500

    soma changed

--- core/settings.py
+++ core/settings.py
@@ -161,14 +161,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / 'static']
-
-else:
-    STATIC_ROOT = BASE_DIR / 'static'
-
-MEDIA_URL ='media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
